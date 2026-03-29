790d612a0089d8dc13ae607a9b80ca3223540607
Renato <sboy63622@gmail.com>
2026-03-29 14:09:21 -0300

    Funções com o Javascript de concluir, criar e remover tarefas finalizadas, e por fim tendo uma espécie de tracking no botão de edição

--- app.js
+++ app.js
@@ -1,1 +1,76 @@
-console.log("Testando...")
+// Seleção de elementos
+const todoForm = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo-input");
+const todoList = document.querySelector("#todo-list");
+const editForm = document.querySelector("#edit-form");
+const editInput = document.querySelector("#edit-input");
+const cancelEditBtn = document.querySelector("#cancel-edit-btn");
+// const searchInput = document.querySelector("#search-input");
+// const eraseBtn = document.querySelector("#erase-button");
+// const filterBtn = document.querySelector("#filter-select");
+// let oldInputValue;
+
+// Funções
+
+const saveTodo = (text) => {
+    const todo = document.createElement("div")
+    todo.classList.add("todo")
+    const todoTitle = document.createElement("h3")
+
+    todoTitle.innerText = text
+    todo.appendChild(todoTitle)
+
+    const doneBtn = document.createElement("button")
+    doneBtn.classList.add("finish-todo")
+    doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>'
+    todo.appendChild(doneBtn)
+
+    const editBtn = document.createElement("button")
+    editBtn.classList.add("edit-todo")
+    editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>'
+    todo.appendChild(editBtn)
+
+    const deleteBtn = document.createElement("button")
+    deleteBtn.classList.add("remove-todo")
+    deleteBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>'
+    todo.appendChild(deleteBtn)
+
+    todoList.appendChild(todo)
+
+    todoInput.value = ""
+    todoInput.focus()
+}
+
+todoForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const inputValue = todoInput.value
+
+    if(inputValue) {
+        // salvando o to do
+        saveTodo(inputValue)
+    }
+})
+
+document.addEventListener("click", (e) => {
+    const targetEl = e.target
+    const parentEl = targetEl.closest("div")
+
+    if (targetEl.classList.contains("finish-todo")){
+        parentEl.classList.toggle("done")
+    }
+
+    if (targetEl.classList.contains("remove-todo"))
+        parentEl.remove()
+
+    if (targetEl.classList.contains("edit-todo"))
+    console.log("Editou!")
+})
+
+// document.addEventListener("click", (e) => {
+//     const targetEl = e.target
+
+//     if (targetEl.classList.contains("edit-todo")){
+//         console.log("Editou a tarefa!")
+//     }
+// })
